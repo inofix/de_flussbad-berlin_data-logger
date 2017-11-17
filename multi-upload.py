71e34d638b2a39bc90a9e653b4629fbe3c3a9d4f
Michael Lustenberger <mic@inofix.ch>
2017-11-17 17:57:56 +0100

    Multi Up
    UI Feedback and archive etc.

--- multi-upload.py
+++ multi-upload.py
@@ -36,6 +36,7 @@ import re
 
 def recalculate_akut(in_file_name):
     from openpyxl import load_workbook
+    print('      <p>Starting import:</p>')
     try:
         # get the data from file (no direct method found..)
         wb_in = load_workbook(in_file_name)
@@ -52,60 +53,111 @@ def recalculate_akut(in_file_name):
 
             measurement = {}
 
-            measurement["analyse"] = ws_in.cell(row=i, column=1).value
-            measurement["location"] = ws_in.cell(row=i, column=2).value
-            measurement["lat"] = ws_in.cell(row=i, column=3).value
-            measurement["long"] = ws_in.cell(row=i, column=4).value
-            measurement["author"] = ws_in.cell(row=i, column=5).value
-            measurement["timestamp"] = ws_in.cell(row=i, column=6).value
+            measurement["analyse"] = str(ws_in.cell(row=i, column=1).value)
+            measurement["location"] = str(ws_in.cell(row=i, column=2).value)
+            # geolocation lat/long are floats
+            measurement["lat"] = float(ws_in.cell(row=i, column=3).value)
+            measurement["long"] = float(ws_in.cell(row=i, column=4).value)
+            measurement["author"] = str(ws_in.cell(row=i, column=5).value)
+            # get a timestamp as a string
+            t = ws_in.cell(row=i, column=6).value
+            if t:
+                t = str(t)
+                try:
+                    # only consider seconds as smallest unit
+                    ti = t.index(".")
+                    t = t[0:ti]
+                except ValueError:
+                    pass
+            if t:
+                try:
+                    # is it a timestamp?
+                    t_ = datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S")
+                    measurement["timestamp"] = t_.isoformat()
+                except ValueError:
+                    # give it another try..
+                    try:
+                        t_ = datetime.datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
+                        measurement["timestamp"] = t_.isoformat()
+                    except ValueError:
+                        # last chance, else really fail..
+                        t_ = datetime.datetime.strptime(t, "%m/%d/%Y %H:%M:%S")
+                        measurement["timestamp"] = t_.isoformat()
 
             for j in range(8, ws_in.max_column):
 
                 vid = ws_in.cell(row=2, column=j).value
-                if vid is not None:
-                    measurement["id"] = vid
-                    measurement["method"] = ws_in.cell(row=3, column=j).value
-                    measurement["name"] = ws_in.cell(row=4, column=j).value
-                    measurement["unit"] = ws_in.cell(row=5, column=j).value
-                    measurement["value"] = ws_in.cell(row=i, column=j).value
+                val = ws_in.cell(row=i, column=j).value
+                if vid:
+                    measurement["id"] = str(vid)
+                    measurement["method"] = str(ws_in.cell(row=3, column=j).value)
+                    measurement["name"] = ws_in.cell(row=4, column=j).value.encode('utf-8')
+                    measurement["unit"] = ws_in.cell(row=5, column=j).value.encode('utf-8')
+                    measurement["value"] = val
 
                     measurements.append(measurement)
+                    print("      <p>Import OK: " + str(vid) + " " +\
+                            measurement["name"] + " = " +\
+                             str(val) + ' ' + measurement["unit"] + '</p>\n')
 
-        # convert to json and just print
+        print('      <p>Import done.</p>')
         return measurements
 
+    except ValueError:
+        print('      <p>Some value could not be imported, please control your input file...</p>\n')
     except Exception:
-        import traceback
-        print(traceback.format_exc())
+        print('      <p>Import failed: the import file does not match the implementation</p>')
 
-def store(storage_path, file_prefix='test', do_archive=False):
+### debug ###
+#    except Exception:
+#        import traceback
+#        print('      <p>')
+#        print(traceback.format_exc())
+#        print('</p>\n')
+
+def store(storage_path, file_prefix='data', do_archive=False):
     timestamp = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
 
-    data_file = storage_path + "/" + file_prefix + ".json"
+    file_prefix += "-"
 
-    response = 'Content-Type: text/plain\n\n' + timestamp + '\n'
+    print('Content-Type: text/html\n\n')
+    print('<html>\n')
+    print('  <body>\n')
+    print('    <h3>Current Upload ' + timestamp + '</h3>\n')
 
     try:
         data = cgi.FieldStorage()
         if data.has_key('filetype') and data.has_key('file'):
-
-            if data['filetype'].value == 'xlsx_akut':
+            file_prefix += data['filetype'].value
+            if data['filetype'].value == 'akut':
+                print('      <p>File received.</p>\n')
                 tmp_file_name = storage_path + '/tmp.xlsx'
                 with open(tmp_file_name, 'w') as of:
                     of.write(data['file'].value)
+                print('      <p>File temporarily stored.</p>\n')
                 m = recalculate_akut(tmp_file_name)
-                with open(data_file, 'w') as of:
+                print('      <p>File converted.</p>\n')
+                data_file_name = storage_path + '/' + file_prefix +\
+                        '-latest.json'
+                with open(data_file_name, 'w') as of:
                     of.write(json.dumps(m))
+                print('      <p>File saved.</p>\n')
+                if do_archive:
+                    data_file_name = storage_path + '/' + file_prefix +\
+                        '-' + timestamp + '.json'
+                    with open(data_file_name, 'w') as of:
+                        of.write(json.dumps(m))
+                    print('      <p>Archive copy saved.</p>\n')
 
-#        file_content = data.value.__repr__()
-#        json_content = json.loads(file_content)
-#        output_content = json.dumps(json_content)
-#        with open(data_file, "w") as of:
-#            of.write(str(output_content))
-        print response
+        print('      <p>Looks all good. Thank you!</p>\n')
+        print('      <p>This is the link to the <a href="/data-store/' +\
+                file_prefix + '-latest.json">result</a></p>\n')
     except:
-        response += 'Please, try again!\n'
-        print response
+        print('      <p>Upload failed. Please try again!</p>\n')
+    print('      <p>This is the link (back) to the ')
+    print('<a href="/data-store/upload.html">upload form</a></p>\n')
+    print('    </body>\n')
+    print('</html>\n')
 
 if __name__ == "__main__":
 
